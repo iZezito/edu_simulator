9119b2bf1c65dfb5939a78b111cdf001a939a6ba
iZezito <emersonsilva81240@gmail.com>
2024-09-15 18:55:17 -0300

    config shadcnui

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className={'container flex justify-center items-center'}>
+          <p>Teste</p>
+      </div>
+    </>
+  )
+}
+
+export default App
