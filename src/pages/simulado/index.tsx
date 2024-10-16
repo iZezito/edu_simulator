0f7cc63d69fe105de0e7e47e289520bb34c14bc9
iZezito <emersonsilva81240@gmail.com>
2024-10-16 15:14:22 -0300

    fix: barra de finalizados

--- src/pages/simulado/index.tsx
+++ src/pages/simulado/index.tsx
@@ -33,7 +33,7 @@ export default function SimuladoPage() {
         </div>
       </div>
       <div className="w-full bg-muted rounded-lg overflow-hidden">
-        <div className="h-2 bg-primary" style={{ width: `${((finalizados|| 0) /15 * 100)}%` }} />
+        <div className="h-2 bg-primary" style={{ width: `${((finalizados || 0) / (data.list?.length || 15) * 100)}%` }} />
       </div>
       
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
